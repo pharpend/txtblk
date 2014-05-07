ed8451778736a48f04b6d14785c9b8f1ff979457
Peter Harpending <pharpend2@gmail.com>
2014-05-06 22:54:00 -0600

    Added a skeleton file.

---
+++ skel/Skel.hs
@@ -0,0 +1,14 @@
+{-|
+Module      : Skel
+Description : A skeleton file
+Copyright   : (c) 2014, Peter Harpending
+License     : BSD3
+Maintainer  : Peter Harpending <pharpend2@gmail.com>
+Stability   : none whatsoever
+Portability : archlinux
+
+This is the description. Add something.
+
+-}
+
+module Skel where
