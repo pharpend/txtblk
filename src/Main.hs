f507afd540d49d8678af8ad06a83c2593fc2d4a6
Peter Harpending <pharpend2@gmail.com>
2014-05-06 22:51:43 -0600

    Initial commit - minimal working build.
    I basically ran `cabal init', and added some stuff so it would build. It doesn't
    actually do anything yet.

---
+++ src/Main.hs
@@ -0,0 +1,18 @@
+{-|
+Module      : Main
+Description : A text blocker
+Copyright   : (c) 2014, Peter Harpending
+License     : BSD3
+Maintainer  : Peter Harpending <pharpend2@gmail.com>
+Stability   : none whatsoever
+Portability : archlinux
+
+This is a text blocker - it blocks sexual texts. It is intended for
+use in other contexts, such as an Andorid app or irssi extension.
+
+-}
+
+module Main where
+
+main :: IO ()
+main = return ()
